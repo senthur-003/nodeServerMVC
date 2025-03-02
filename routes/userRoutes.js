7a041f973fd2b0669dc034d4930331961f65f67f
senthur-003 <senthur.general@gmail.com>
2025-03-02 16:43:45 +0530

    san

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const router = express.Router();
-const { fetchAdminUserDetails, fetchUserProfile,updateUser_Profile,addUser,fetchMenuList } = require('../controllers/userController');
+const { fetchAdminUserDetails, fetchUserProfile, updateUser_Profile, addUser, fetchMenuList } = require('../controllers/userController');
 const verifyToken = require('../token_verification');
 
 
 router.get('/admin', fetchAdminUserDetails);
 router.get('/getProfile', fetchUserProfile);
-router.post('/addUser',verifyToken, addUser);
-router.post('/userProfile',verifyToken,updateUser_Profile);
-router.get('/getMenu',fetchMenuList);
-
-
+router.post('/addUser', verifyToken, addUser);
+router.post('/userProfile', verifyToken, updateUser_Profile);
+router.get('/getMenu', fetchMenuList);
 
 module.exports = router;
